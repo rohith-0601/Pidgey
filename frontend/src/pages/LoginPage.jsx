f5fcb2af3dc0419092cae98bbd31969ae84902c9
rohith <rohithperugu3@gmail.com>
2025-07-18 23:12:54 +0530

    new start

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,18 +2,10 @@ import React, { useEffect } from 'react';
 import WorldMap from "../components/WorldMap";
 import LoginBox from "../components/LoginBox";
 import { useNavigate } from 'react-router-dom';
-import { handleRedirectResult } from '../utils/utils';
+
 
 function LoginPage() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    handleRedirectResult().then((user) => {
-      if (user) {
-        navigate("/home", { state: { user } });
-      }
-    });
-  }, []);
+  
 
   return ( 
     <div className="position-relative vh-100 vw-100 overflow-hidden bg-white">
